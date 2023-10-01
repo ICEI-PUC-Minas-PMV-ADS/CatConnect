8561ac4ee282810c0ff47448d75d299c7970e49f
Nayara <nayarakatlin@yahoo.com.br>
2023-10-01 14:42:30 -0300

    Inclusão de novas rotas

--- src/app/frontend/src/App.js
+++ src/app/frontend/src/App.js
@@ -2,6 +2,12 @@ import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import Cards from "./pages/Home/Cards";
+import Dashboard from "./pages/Dashboard";
+import NovoGato from "./pages/NovoGato";
+import Registros from "./pages/Registros/Registros";
+import Adocoes from "./pages/Adocoes";
+import Users from "./pages/Users";
+import Configuracoes from "./pages/Configuracoes/Configuracoes";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function App() {
@@ -10,6 +16,13 @@ export default function App() {
       <Routes>
         <Route exact path="/login" element={<LoginPage />} />
         <Route exact path="/" element={<Cards />} />
+        <Route exact path="/dashboard" element={<Dashboard />} />
+        <Route exact path="/novoGato" element={<NovoGato />} />
+        <Route exact path="/registros" element={<Registros />} />
+        <Route exact path="/adocoes" element={<Adocoes />} />
+        <Route exact path="/users" element={<Users />} />
+        <Route exact path="/configuracoes" element={<Configuracoes />} />
+
       </Routes>
     </BrowserRouter>
   );
