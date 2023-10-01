7dd9ff91143758b9d8e8e0d6ff8014c353f4991c
Edu-DevBR <440744@sga.pucminas.br>
2023-10-01 20:12:08 -0300

    Componentes Usuario

---
+++ src/app/frontend/src/pages/Users/UserManagement.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import UserList from "./UserList";
+import UserForm from "./UserForm";
+
+function UserManagement() {
+  return (
+    <div>
+      <UserList />
+      <UserForm />
+    </div>
+  );
+}
+
+export default UserManagement;
