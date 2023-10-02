4df685b2dd3c09a21277ef7ff6548ade4b524f02
Mariane03 <marianeserapiaoa@gmail.com>
2023-10-01 22:03:49 -0300

    Corrigindo sidebar duplicado

--- src/app/frontend/src/App.js
+++ src/app/frontend/src/App.js
@@ -3,15 +3,10 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import Home from "./pages/Home/Home";
 import "react-toastify/dist/ReactToastify.css";
-import { useCookies } from "react-cookie"; // Import useCookies hook
-import Sidebar from "./components/Sidebar/Sidebar";
 
 export default function App() {
-  const [cookies] = useCookies();
-
   return (
     <div className="app-container">
-      {cookies.jwt && window.location.pathname !== "/login" && <Sidebar />}
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
