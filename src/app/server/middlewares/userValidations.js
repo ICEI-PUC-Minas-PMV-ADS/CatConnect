3b5447b866681d98592977aedfa24a0370f4c3d9
Edu-DevBR <440744@sga.pucminas.br>
2023-10-12 03:29:28 -0300

    Resolvido erro de digitação

--- src/app/server/middlewares/userValidations.js
+++ src/app/server/middlewares/userValidations.js
@@ -1,18 +1,18 @@
-const { body } = reqire("express-validator")
+const { body } = require("express-validator")
 
 const userCreateValidation = () => {
-    retur[
+    return[
         body("name")
-            .isString().withNessage("O campo nome é obrigatório.")
-            .isLength({ min: 3 }).withNessage("O nome deve ter no mínimo 3 caracteres."),
+            .isString().withMessage("O campo nome é obrigatório.")
+            .isLength({ min: 3 }).withMessage("O nome deve ter no mínimo 3 caracteres."),
         body("email")
-            .isString().withNessage("O campo email é obrigatório.")
-            .isEmail().withNessage("Insira um email válido."),
+            .isString().withMessage("O campo email é obrigatório.")
+            .isEmail().withMessage("Insira um email válido."),
         body("password")
-            .isString().withNessage("O campo senha é obrigatório.")
-            .isLength({ min: 6 }).withNessage("A sua senha deve ter no mínimo 6 caracteres."),
+            .isString().withMessage("O campo senha é obrigatório.")
+            .isLength({ min: 6 }).withMessage("A sua senha deve ter no mínimo 6 caracteres."),
         body("matchpassword")
-            .isString().withNessage("A confirmação da senha é obrigatória.")
+            .isString().withMessage("A confirmação da senha é obrigatória.")
             .custom((value, { req }) => {
                 if (value != req.body.password) {
                     throw new Error("As senhas não digitadas não são iguais.");
