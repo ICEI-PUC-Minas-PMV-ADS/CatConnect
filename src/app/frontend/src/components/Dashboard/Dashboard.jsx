2dba2ebf739f28ef3367c642c199650df794de13
Nayara <nayarakatlin@yahoo.com.br>
2023-10-18 16:17:58 -0300

    Nay-Criação grids dashboard

--- src/app/frontend/src/components/Dashboard/Dashboard.jsx
+++ src/app/frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import "../Dashboard/Dashboard.css";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
 
 const Dashboard = () => {
   return (
-  <div>
-    <h1>Dashboard</h1>
-    </div>
+    <Box>
+      <h1> Dashboard</h1>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={4}>
+          <Grid item xs={8}>
+          <div className="ResumoBox">
+            <p>Grid1</p>
+            </div>
+          </Grid>
+
+          <Grid item xs={8}>
+          <p>Grid2</p>
+          </Grid>
+
+          <Grid item xs={8}>
+          <p>Grid3</p>
+          </Grid>
+
+          <Grid item xs={4}>
+          <p>Grid4</p>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
   )
 };
 
