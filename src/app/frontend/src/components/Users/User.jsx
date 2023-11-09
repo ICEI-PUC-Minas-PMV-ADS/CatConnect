a44504a8b8cbc73b2893b34a959601fd6276f7e9
Nayara <nayarakatlin@yahoo.com.br>
2023-11-08 23:38:43 -0300

    Atualização front tela usuários

--- src/app/frontend/src/components/Users/User.jsx
+++ src/app/frontend/src/components/Users/User.jsx
@@ -12,6 +12,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import DeleteForeverSharpIcon from '@mui/icons-material/DeleteForeverSharp';
 import Swal from 'sweetalert2';
+import AddIcon from '@mui/icons-material/Add';
 
 const Usuarios = () => {
   const [page, setPage] = useState(0);
@@ -256,7 +257,7 @@ const Usuarios = () => {
 
         </div>
         <div className="user-linha">
-          <div id="user-table">
+          <div className="user-table">
 
             <div style={{ display: "flex", alignItems: "center" }}>
               <div style={{ margin: "10px" }}>
@@ -270,9 +271,14 @@ const Usuarios = () => {
                 />
               </div>
               <div style={{ marginLeft: "auto", marginRight: "10px" }}>
-                <Button id="add-user" onClick={() => abrirAddUsuario()}>
-                  <span style={{ fontSize: "24px", color: "white" }}>+</span>
-                </Button>
+              <Button
+                        variant="contained"
+                        color="success"
+                        onClick={abrirAddUsuario}
+                        startIcon={<AddIcon style={{color: 'white'}}/>}
+                    >
+                        Adicionar
+                    </Button>
               </div>
             </div>
 
